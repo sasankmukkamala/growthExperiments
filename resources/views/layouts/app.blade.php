c4fcbcc2136e155c8299dffd8b6babdbed845e4e
Tiago Pereira <wildlifechorus@gmail.com>
2016-11-16 11:31:36 +0000

    Hide search bar if user is not logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,16 +46,18 @@
                         &nbsp;
                     </ul>
 
-                    {!! Form::open(['method' => 'GET', 'route' => 'experiments.search', 'role' => 'search']) !!}
-                    <div class="col-lg-6">
-                        <div class="input-group">
-                            <input type="text" name="search" class="form-control" placeholder="Search for...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="submit">Search!</button>
-                            </span>
-                        </div><!-- /input-group -->
-                    </div><!-- /.col-lg-6 -->
-                    {!! Form::close() !!}
+                    @if (Auth::user())
+                        {!! Form::open(['method' => 'GET', 'route' => 'experiments.search', 'role' => 'search']) !!}
+                        <div class="col-lg-6">
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control" placeholder="Search for...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="submit">Search!</button>
+                                </span>
+                            </div><!-- /input-group -->
+                        </div><!-- /.col-lg-6 -->
+                        {!! Form::close() !!}
+                    @endif
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
